00ee482d234823835a93fe9a5d4fff016896a91f
Dan Phiffer <dan@phiffer.org>
2022-01-24 09:27:24 -0500

    update version to 0.0.2

--- wp-shlink.php
+++ wp-shlink.php
@@ -13,7 +13,7 @@
  * Description:       Create and manage Shlink short links from WordPress
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.0.1
+ * Version:           0.0.2
  * Author:            The Markup
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
