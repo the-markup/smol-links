4cb707e04b337a295b536b755b3e616436ab4c0f
Dan Phiffer <dan@phiffer.org>
2022-01-28 17:03:10 -0500

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// See also: https://dev.to/alexstandiford/make-webpack-configuration-easy-with-wordpress-scripts-26kk
+const defaultConfig = require('@wordpress/scripts/config/webpack.config.js');
+const path = require('path');
+
+module.exports = {
+	...defaultConfig,
+	...{
+		entry: {
+			editor: path.resolve(process.cwd(), 'src', 'js', 'editor.js'),
+			manager: path.resolve(process.cwd(), 'src', 'js', 'manager.js')
+		},
+		performance: {
+			hints: false,
+			maxEntrypointSize: 512000,
+			maxAssetSize: 512000
+		}
+	}
+};
